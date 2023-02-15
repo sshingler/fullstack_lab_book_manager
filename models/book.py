8b97b10a3354dcbade2c3301241dc685fe3d303f
SShingler <s.shingler@hotmail.co.uk>
2023-02-15 16:28:45 +0000

    1 - file structure

---
+++ models/book.py
@@ -0,0 +1,8 @@
+class Book:
+   def __init__(self, title, genre, publisher, id = None):
+    self.title = title 
+    self.genre = genre
+    self.publisher = publisher
+    self.id = id
+    
+     
