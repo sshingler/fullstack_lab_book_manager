60d837c92ddc691a9b66cb63e2e39194b46103a5
SShingler <s.shingler@hotmail.co.uk>
2023-02-15 21:58:23 +0000

    4 - view all books, view select book, delete book

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -10,3 +10,14 @@ def books():
     books = book_repository.select_all()
     return render_template("books/index.html", all_books = books)
 
+@books_blueprint.route("/books/<id>", methods=['GET'])
+def show_book(id):
+    book = book_repository.select(id)
+    return render_template('books/show.html', book = book)
+
+
+@books_blueprint.route("/books/<id>/delete", methods=['POST'])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect('/books')
+
